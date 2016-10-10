488a818838944a3168b895fbe833a80e2a074be1
claupa <claupa.plasencia@gmail.com>
2016-10-10 16:30:23 -0400

    update base template with login & logout links

--- crmapp/suscribers/views.py
+++ crmapp/suscribers/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
+from .models import Subscriber
 
 from .forms import SubscriberForm
 
--- crmapp/urls.py
+++ crmapp/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from marketing.views import HomePage
 from crmapp.suscribers.views import subscriber_new
+import django.contrib.auth.views as djauth
 
 urlpatterns = [
     # Marketing pages
@@ -29,8 +30,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     # Login/Logout URLs
-    (r'^login/$','django.contrib.auth.views.login', {'template_name': 'login.html'}),
-    (r'^logout/$','django.contrib.auth.views.logout', {'next_page': '/login/'}),
+    url(r'^login/$', djauth.login, {'template_name': 'login.html'}),
+    url(r'^logout/$',djauth.logout, {'next_page': '/login/'}),
 
     # Account related URLs
 
