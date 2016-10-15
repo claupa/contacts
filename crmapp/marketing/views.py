73d8842f467fbb5c4dbc831a3c316474d4bce351
claupa <claupa.plasencia@gmail.com>
2016-10-14 21:08:26 -0400

    refactor marketing views

--- crmapp/marketing/views.py
+++ crmapp/marketing/views.py
@@ -6,24 +6,7 @@ from django.contrib.auth.decorators import login_required
 from crmapp.oncuba.forms import FilterForm
 from django.db.models import Q
 
-
-def check_list(categorias, proyectos, contacts):
-    possible_contacts = []
-    if categorias:
-        for categoria in categorias:
-            for contact in contacts:
-                if contact.categoria.filter(pk=categoria).exists() and not contact in possible_contacts:
-                    possible_contacts.append(contact)
-        contacts= possible_contacts
-        possible_contacts = []
-    if proyectos:
-        for proyecto in proyectos:
-            for contact in contacts:
-                if contact.proyecto.filter(pk=proyecto).exists() and not contact in possible_contacts:
-                    possible_contacts.append(contact)
-        contacts=possible_contacts
-    return contacts
-
+@login_required()
 def home_page(request, template='marketing/home.html'):
     contact_person = Persona.objects.all()
     contact_entidad = Entidad.objects.all()
@@ -54,77 +37,72 @@ def home_page(request, template='marketing/home.html'):
                     contact_person = []
     else:
         filter_form = FilterForm()
-    contacts =[]
-    index = 0
-    for persona in contact_person:
-        index +=1
-        contacts.append({'index' : index,
-                        'nombre' : persona.nombre+ ' '+ persona.apellidos,
-                        'ocupacion': persona.ocupacion + '/'+ persona.lugar_de_trabajo,
-                        'created_by': persona.created_by.user.username,
-                        'can_edit': persona.created_by.user.username == request.user.username or request.user.is_superuser,
-                        'can_read': check_credentials(persona, request.user),
-                        'can_delete': persona.created_by.user.username == request.user.username or request.user.is_superuser,
-                        'is_persona' : True
-                        })
-        if contacts[-1]['can_read']:
-            contacts[-1]['id'] = persona.pk 
-            # print contacts[-1]
-    for entidad in contact_entidad:
-        index +=1 
-        contacts.append({'index' : index,
-                        'nombre' : entidad.nombre,
-                        'ocupacion': entidad.servicios,
-                        'created_by': entidad.created_by.user.username,
-                        'can_edit': entidad.created_by.user.username == request.user.username or request.user.is_superuser,
-                        'can_read': check_credentials(entidad, request.user),
-                        'can_delete': entidad.created_by.user.username == request.user.username or request.user.is_superuser,
-                        'is_persona' : False
-                         })
-        if contacts[-1]['can_read']:
-            contacts[-1]['id'] = entidad.pk 
+    
+    contacts = get_contact_info(contact_person, request.user, True)
+    contacts.extend(get_contact_info(contact_entidad, request.user, False))
 
-    return render(request, template, {'s': s,'filter_form':filter_form, 'categorias' : Categoria.objects.all(),
+    return render(request, template, {'s': s,
+                                    'filter_form':filter_form, 
+                                    'categorias' : Categoria.objects.all(),
                                     'proyectos': Proyecto.objects.all(),
                                     'contacts': contacts})
 
-
-
 @login_required()
 def mis_contactos(request, template= "marketing/mis_contactos.html"):
-    
     db_personas= Persona.objects.filter(created_by__user__username=request.user.username)
     db_entidades= Entidad.objects.filter(created_by__user__username=request.user.username)
-    print db_personas.count()
-    print db_entidades.count()
 
-    contacts =[]
-    index = 0
-    for persona in db_personas:
-        index +=1 
-        contacts.append({'index' : index,
-                        'nombre' : persona.nombre+ ' '+ persona.apellidos,
-                        'ocupacion': persona.ocupacion + '/'+ persona.lugar_de_trabajo,
-                        'created_by': persona.created_by.user.username,
-                        'can_edit': persona.created_by.user.username == request.user.username or request.user.is_superuser,
-                        'can_read': check_credentials(persona, request.user),
-                        'can_delete': persona.created_by.user.username == request.user.username or request.user.is_superuser,
-                        'is_persona': True })
-        if contacts[-1]['can_read']:
-            contacts[-1]['id'] = persona.pk 
-            # print contacts[-1]
-
-    for entidad in db_entidades:
-        index +=1 
-        contacts.append({'index' : index,
-                        'nombre' : entidad.nombre,
-                        'ocupacion': entidad.servicios,
-                        'created_by': entidad.created_by.username,
-                        'can_edit': entidad.created_by.user.username == request.user.username or request.user.is_superuser,
-                        'can_read': check_credentials(entidad, request.user),
-                        'can_delete': entidad.created_by.user.username == request.user.username or request.user.is_superuser,
-                        'is_persona': False})
-        if contacts[-1]['can_read']:
-            contacts[-1]['id'] = entidad.pk 
+    contacts = get_contact_info(db_personas, request.user)
+    contacts.extend(get_contact_info(db_entidades, request.user, False))
 
     return render(request, template, {'contacts': contacts})
+
+
+def check_list(categorias, proyectos, contacts):
+    possible_contacts = []
+    if categorias:
+        for categoria in categorias:
+            for contact in contacts:
+                if contact.categoria.filter(pk=categoria).exists() and not contact in possible_contacts:
+                    possible_contacts.append(contact)
+        contacts= possible_contacts
+        possible_contacts = []
+    if proyectos:
+        for proyecto in proyectos:
+            for contact in contacts:
+                if contact.proyecto.filter(pk=proyecto).exists() and not contact in possible_contacts:
+                    possible_contacts.append(contact)
+        contacts=possible_contacts
+    return contacts
+
+def get_contact_info(contacts, user, persona = True):
+    new_contacts = []
+    index = 0
+    if persona:
+        fn = get_name_and_ocupation_of_persona
+    else:
+        fn = get_name_and_ocupation_of_entidad
+
+    for contact in contacts:
+        index +=1
+        creator = contact.created_by.user
+        is_owner_or_admin = creator == user or user.is_superuser
+        
+        contact = {'index' : index,
+                    'nombre' : fn(contact),
+                    'ocupacion': fn(contact),
+                    'created_by': creator.username,
+                    'can_read': check_credentials(contact, user),
+                    'can_edit': is_owner_or_admin,
+                    'can_delete': is_owner_or_admin,
+                    'is_persona': True }
+        if contact['can_read']:
+            contact['id'] = contact.pk
+        new_contacts.append(contact)
+    return new_contacts
+
+def get_name_and_ocupation_of_persona(contact):
+    return (contact.nombre + ' ' + contact.apellidos , contact.ocupacion+'/'+contact.lugar_de_trabajo)
+
+def get_name_and_ocupation_of_entidad(contact):
+    return (contact.nombre, contact.servicios)
