174622294bf88e393ea49e87be86fc88d3a6495a
claupa <claupa.plasencia@gmail.com>
2016-11-23 15:23:16 -0500

    Deleting accounts app.

--- crmapp/accounts/urls.py
+++ crmapp/accounts/urls.py
@@ -1,7 +1,3 @@
 from django.conf.urls import patterns, url
-from crmapp.accounts.views import account_detail, account_cru
 
 
-account_urls =[url(r'^$', account_detail , name='account_detail'),
-url(r'^edit/$',account_cru, name='account_update'
-    ),]
